6e4f857935c14fd7c301041f68e5cb4a87ca4d95
Z User <z@container>
2026-06-23 08:25:38 +0000

    bdd65ea6-cb98-4c45-894a-fa410ec861e5

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
